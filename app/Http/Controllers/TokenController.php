5d5337550764837de56703aedc25014fcfdf3e5d
bilalsajid-eazisols <bilal.sajid@eazisols.com>
2025-02-20 20:24:26 +0500

    add getTokenData api for getting token data to show on swap : Client side

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -13,4 +13,41 @@ class TokenController extends Controller
         $category = '';
         return view('admin.tokensetting', compact('category','tokens'));
     }
+
+    public function getTokenData(Request $request)
+    {
+        $tokenName = $request->query('token');
+
+        if (!$tokenName) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token name is required',
+            ], 400);
+        }
+
+        $token = token::where('name', $tokenName)
+            ->select(
+                'id as tokenId',
+                'membershipclub_id',
+                'name',
+                'logo',
+                'symbol',
+                'token_conversion_rate',
+                'initialsupply',
+                'totalsupply'
+            )
+            ->first();
+
+        if (!$token) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Token not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $token,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
     use App\Http\Controllers\contactcontroller;
     use App\Http\Controllers\MembershipclubController;
     use App\Http\Controllers\usercontroller;
+    use App\Http\Controllers\TokenController;
     use Illuminate\Support\Facades\Route;
 
     // route::get('settings',[SettingController::class,'index'])->middleware('auth:sanctum');
@@ -15,6 +16,9 @@
     route::get('membershipclub/list',[MembershipclubController::class,'listing'])->middleware('auth:sanctum');
     route::get('membershipclub/public-view/{id}',[MembershipclubController::class,'publicview']);
     route::get('membershipclub/details/{id}',[MembershipclubController::class,'detailsview'])->middleware('auth:sanctum');
+    route::get('token/data',[TokenController::class,'getTokenData']);
+    route::get('token/getAllTokenData',[TokenController::class,'getAllTokensData']);
+
 
     route::get('membershipclub/joining/{id}',[MembershipclubController::class,'joinmembershipclub'])->middleware('auth:sanctum');;
 route::post('membershipclub/comment/save/{id}',[MembershipclubController::class,'savecomment'  ])->middleware('auth:sanctum');
